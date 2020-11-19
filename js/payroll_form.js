60237af05e7057c678ffacafe018f75cd8cfa358
mamudsamiul <samiulmamud1995@gmail.com>
2020-11-19 08:18:11 +0530

    (Samiul Mamud) Added: Ability to Perform Validation Employee Payroll Data setter methods

--- js/payroll_form.js
+++ js/payroll_form.js
@@ -5,6 +5,14 @@ salary.addEventListener('input', function(){
   output.textContent = salary.value;
 });
 
+const text = document.querySelector("#name");
+const textError = document.querySelector(".text-error");
+const nameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$");
+text.addEventListener("input", function () {
+  if (nameRegex.test(text.value)) textError.textContent = "";
+  else textError.textContent = "Name is Incorrect";
+});
+
 function save(){
   var name= document.getElementById("name").value;
   var picture = document.querySelector('input[name = profile]:checked').value;
